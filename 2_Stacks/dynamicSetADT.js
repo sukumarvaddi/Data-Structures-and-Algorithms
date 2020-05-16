64b099bbc02cf26dcc9ed417de3cc098ef5f5388
sukumar Vaddi <svaddi_July_1_2015@macbook-pro.attlocal.net>
2020-05-16 09:47:08 -0500

    exporting dynamicset

--- 2_Stacks/dynamicSetADT.js
+++ 2_Stacks/dynamicSetADT.js
@@ -13,18 +13,10 @@ function DynamicSet() {
         return elementHolder;
     };
     this.isIn = function (element) {
-        return Boolean(~elementHolder.includes(element));
+        return elementHolder.includes(element);
     };
 
     return this;
 }
 
-var ds = new DynamicSet();
-ds.insert(5);
-ds.insert(8);
-ds.insert(-1);
-ds.insert(0);
-ds.insert(9);
-
-console.log(ds.delete(8));
-console.log(ds.delete(8));
+exports.DynamicSet = DynamicSet;
