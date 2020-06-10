24ab04e29666bb37e1b60199d78aae2e569538cb
sukumar Vaddi <svaddi_July_1_2015@macbook-pro.attlocal.net>
2020-06-10 07:07:44 -0500

    queue with ll

--- 3_QueuesAndLinkedLists/src/3_queuesWithSinglyLinkedList/queue.js
+++ 3_QueuesAndLinkedLists/src/3_queuesWithSinglyLinkedList/queue.js
@@ -1,3 +1,30 @@
 const linkedList = require('../1_singlyLinkedList/singlyLinkedList');
 
-function queue() {}
+function queue() {
+    return {
+        enqueue,
+        dequeue,
+        front,
+        size,
+        isEmpty,
+    };
+
+    function enqueue(el) {
+        linkedList.insertAtEnd(el);
+    }
+    function dequeue() {
+        linkedList.deleteNodeAtGivenPosition(0);
+    }
+
+    function front() {
+        linkedList.getAtHead();
+    }
+
+    function size() {
+        linkedList.getSize();
+    }
+
+    function isEmpty() {
+        return linkedList.getSize() === 0;
+    }
+}
